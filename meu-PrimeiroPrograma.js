61f039e7b508c2aaf4ef6e98ec87b1b3721ed9b0
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-02-07 21:35:21 -0300

    variaveis

---
+++ meu-PrimeiroPrograma.js
@@ -0,0 +1,8 @@
+console.log('Meu Primeiro programa : trabalahndo com variavesi')
+
+const idade = 26
+console.log(idade)
+console.log(idade + 2)
+
+const idadeSomada = idade + 2
+console.log(idadeSomada)
